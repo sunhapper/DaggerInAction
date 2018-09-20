db36f3fd292766532662f5870d57914acb16fddc
sunhapper <haipeng.sun3@56qq.com>
2018-09-20 15:30:45 +0800

    Lazy Provider
    - Lazy.get()多次获取的对象是同一个
    - Provider.get()多次获取的对象要根据是否是局部单例（局部单例是同一个）和提供对象的实现（@Provides方法可以每次都返回同一个实例）决定是否是同一个

--- app/src/main/java/me/sunhapper/dagger/daggerinaction/activity/SimpleActivity.java
+++ app/src/main/java/me/sunhapper/dagger/daggerinaction/activity/SimpleActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 
+import dagger.Lazy;
 import dagger.sunhapper.me.daggerinaction.R;
 import me.sunhapper.dagger.daggerinaction.bean.SimpleActivityBean;
 import me.sunhapper.dagger.daggerinaction.bean.SimpleInjectBean;
@@ -31,6 +33,14 @@ public class SimpleActivity extends AppCompatActivity {
     SimpleActivityBean mSimpleActivityBean;
     @Inject
     SimpleModuleBean mSimpleModuleBean;
+    @Inject
+    Lazy<SimpleInjectBean> mSimpleInjectBeanLazy;
+    @Inject
+    Lazy<SimpleModuleBean> mSimpleModuleBeanLazy;
+    @Inject
+    Provider<SimpleInjectBean> mSimpleInjectBeanProvider;
+    @Inject
+    Provider<SimpleModuleBean> mSimpleModuleBeanProvider;
     private TextView mTvInfo;
     private SimpleComponent mSimpleComponent;
 
@@ -44,11 +54,15 @@ public class SimpleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple);
         initView();
-        mTvInfo.setText(
-                String.format("%s\n%s\n%s\n\n%s\n%s\n\n%s\n%s", mSimpleInjectBean,
-                        mSimpleInjectBean.mSimpleInjectExtraBean,
-                        mSimpleInjectExtraBean, mSimpleModuleBean, mSimpleInjectBean.mSimpleModuleBean,
-                        mSimpleModuleActivityBean, mSimpleActivityBean));
+        String step1 = String.format("%s\n%s\n%s\n\n%s\n%s\n\n%s\n%s\n\n", mSimpleInjectBean,
+                mSimpleInjectBean.mSimpleInjectExtraBean,
+                mSimpleInjectExtraBean, mSimpleModuleBean, mSimpleInjectBean.mSimpleModuleBean,
+                mSimpleModuleActivityBean, mSimpleActivityBean);
+        String step2 = "Lazy:\n" + mSimpleInjectBeanLazy.get() + "\n" + mSimpleInjectBeanLazy.get() + "\n"
+                + mSimpleModuleBeanLazy.get() + "\n" + mSimpleModuleBeanLazy.get() + "\n";
+        String step3 = "Provider:\n" + mSimpleInjectBeanProvider.get() + "\n" + mSimpleInjectBeanProvider.get() + "\n"
+                + mSimpleModuleBeanProvider.get() + "\n" + mSimpleModuleBeanProvider.get();
+        mTvInfo.setText(step1 + step2 + step3);
     }
 
     private void initView() {
